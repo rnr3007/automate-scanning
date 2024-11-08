2a5081d29cdb2b35f755e5e9739d66c1aaf94ea6
rnr3007 <rivarahim@gmail.com>
2024-11-08 04:28:40 -0500

    Adding command to be automated

--- automate.sh
+++ automate.sh
@@ -3,32 +3,39 @@
 # Check the working directory
 WORK_DIR=$(pwd)
 
-# Automate subfinder scan, nmap, and nuclei for list of csirt enabled website
-while read -r domain; do
-    echo "Automating check for: $line"
+# Automate subfinder scan, nmap, and nuclei for li.st of csirt enabled website
 
-    # Check if directory exists, if not, create the new one
-    if ! [[ -d ./$domain ]]; then
-        mkdir $domain
-    fi
+if [[ -z $1 ]]; then
+    while read -r domain; do
+        if [[ -z $domain ]]; then
+            break
+        fi
 
-    # Working in the directory
-    cd ./$domain
+        echo "Automating check for: $line"
 
-    echo "==================="
-    echo "Starting subfinder"
-    echo "==================="
-    subfinder -d $domain -o subfinder.txt
+        # Check if directory exists, if not, create the new one
+        if ! [[ -d ./$domain ]]; then
+            mkdir $domain
+        fi
 
-    echo "==================="
-    echo "Starting nuclei"
-    echo "==================="
-    nuclei -l subfinder.txt -t ~/nuclei-templates/ -o nuclei-full.txt
+        # Working in the directory
+        cd ./$domain
 
-    echo "==================="
-    echo "Starting httpx"
-    echo "==================="
-    httpx -l subfinder.txt -silent -o httpx.txt -fc 200
+        echo "==================="
+        echo "Starting subfinder"
+        echo "==================="
+        subfinder -d $domain -o subfinder.txt
 
-    cd $WORKDIR
-done
+        echo "==================="
+        echo "Starting nuclei"
+        echo "==================="
+        nuclei -l subfinder.txt -t ~/nuclei-templates/ -o nuclei-full.txt
+
+        echo "==================="
+        echo "Starting httpx"
+        echo "==================="
+        httpx -l subfinder.txt -silent -o httpx.txt -fc 200
+
+        cd $WORKDIR
+    done
+fi
